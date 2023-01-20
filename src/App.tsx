865f73dc879b89211cb14d9dcb2c07675b195301
Pranjal Yadav <2019uec1008@mnit.ac.in>
2023-01-20 19:34:57 +0530

    ✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { UserList } from "./pages/UserList";
+import { generateDummyUsers } from "./utils";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-full">
+      <h1 className="text-2xl font-bold backdrop-brightness-100 text-center my-4">
+        Techwondoe Assignment
+      </h1>
+      <div className="grid place-items-center">
+        <UserList></UserList>
+      </div>
     </div>
   );
 }
 
-export default App;
+export { App };
